b7432f9a9c4770ef2ba610a22555a54d54bba1de
Bayram AKGÜL <bayramakgul@gmail.com>
2025-03-10 13:50:59 +0300

    #Hafta2 Eklendi

---
+++ MauiAppLayaout/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppLayaout;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
