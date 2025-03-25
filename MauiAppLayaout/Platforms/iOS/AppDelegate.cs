c8428f6fef849ff1a9a620cd5ab4803e9df23667
Bayram AKGÜL <bayramakgul@gmail.com>
2025-03-25 08:50:39 +0300

    #gereksizler silindi

--- MauiAppLayaout/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiAppLayaout;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
