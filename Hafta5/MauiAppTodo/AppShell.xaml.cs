e93db4975212f2707d0eebed1f1c168b4619c131
Bayram AKGÜL <bayramakgul@gmail.com>
2025-04-14 13:55:52 +0300

    #vize sonrası ilk uygulama #INotifyPropertyChanged arayüzü uygulanacak

---
+++ Hafta5/MauiAppTodo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppTodo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hafta5/MauiAppTodo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppTodo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
